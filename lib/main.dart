8a5e5987978f5aa8888068cc617f82890cf22f48
Paras Shenmare <shenmareparas@gmail.com>
2023-05-03 16:16:41 +0530

    debug banner turned off

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Center(
