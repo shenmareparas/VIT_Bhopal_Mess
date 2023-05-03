055817d3058249645579cb654551dc92d107b8d8
Paras Shenmare <shenmareparas@gmail.com>
2023-05-03 16:04:59 +0530

    fresh commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'VIT Bhopal Mess',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: PhotoView(
+          imageProvider: const AssetImage('assets/images/timetable.jpg'),
+        ),
+      ),
+    );
+  }
+}
