738a7473331546c4fa751ddd2cffe1c37f40106f
Paras Shenmare <shenmareparas@gmail.com>
2023-05-05 00:52:34 +0530

    Data Map Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,225 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_print
 
-// Step 1: Define your data model
-class MyData {
-  final String name;
-  final DateTime time;
-
-  MyData({required this.name, required this.time});
-}
-
-// Step 2: Build your UI
-class MyHomePage extends StatelessWidget {
-  final Map<String, List<MyData>> allData;
-
-  const MyHomePage({Key? key, required this.allData}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // Step 3: Add time range logic
-    final currentTime = DateTime.now();
-    final weekday = _getWeekdayString(currentTime.weekday);
-    final timeRanges = allData[weekday] ?? [];
-
-    //final filteredData =
-    //    timeRanges.where((data) => data.time.isBefore(currentTime)).toList();
-    final filteredData = timeRanges;
-
-    // Step 5: Display filtered data
-    return Scaffold(
-      appBar: AppBar(
-          title: const Center(
-              child: Text(
-            'VIT Bhopal Mess',
-            style: TextStyle(color: Colors.white),
-          )),
-          backgroundColor: Colors.black),
-      body: ListView.builder(
-        itemCount: filteredData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text(filteredData[index].name),
-          );
-        },
-      ),
-    );
-  }
-
-  // Helper method to convert weekday number to string
-  String _getWeekdayString(int weekday) {
-    switch (weekday) {
-      case DateTime.monday:
-        return 'Monday';
-      case DateTime.tuesday:
-        return 'Tuesday';
-      case DateTime.wednesday:
-        return 'Wednesday';
-      case DateTime.thursday:
-        return 'Thursday';
-      case DateTime.friday:
-        return 'Friday';
-      case DateTime.saturday:
-        return 'Saturday';
-      case DateTime.sunday:
-        return 'Sunday';
-      default:
-        return '';
-    }
-  }
-}
-
-// Create some sample data
-final Map<String, List<MyData>> allData = {
-  'Monday': [
-    MyData(name: 'Item 1', time: DateTime(2023, 5, 1, 10, 0)),
-    MyData(name: 'Item 2', time: DateTime(2023, 5, 1, 15, 0)),
-  ],
-  'Tuesday': [
-    MyData(name: 'Item 3', time: DateTime(2023, 5, 2, 12, 0)),
-  ],
-  'Wednesday': [
-    MyData(name: 'Item 4', time: DateTime(2023, 5, 3, 11, 0)),
-  ],
-  'Thursday': [
-    MyData(name: 'Breakfast', time: DateTime(2023, 5, 4, 14, 0)),
-  ],
-  'Friday': [
-    MyData(name: 'Item 6', time: DateTime(2023, 5, 5, 14, 0)),
-  ],
-  'Saturday': [
-    MyData(name: 'Item 7', time: DateTime(2023, 5, 6, 11, 0)),
-  ],
-  'Sunday': [
-    MyData(name: 'Item 8', time: DateTime(2023, 5, 7, 13, 0)),
-  ],
-};
-
-// Display the home page
-// Step 6: Create a main method to run the app
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(allData: allData),
-    );
-  }
+  var days = {
+    'Monday': {
+      'Breakfast': [
+        "Idly, Midhu Vada",
+        "Chutney, Sambar",
+        "Banana/Fruit Salad",
+        "Bread",
+        "Butter/Jam",
+        "Tea, Coffee, Milk"
+      ],
+      'Lunch': [
+        'Salad',
+        'Roti',
+        "Rajma Gharwala",
+        "Jeera Rice",
+        "Aloo Pepper Fry",
+        "Rice",
+        "Lemon Rice",
+        "Koottu",
+        "Rasam",
+        "Juice"
+      ],
+      'Evening Snacks': ["Vada Pav", "Tea, Coffee, Milk"],
+      'Dinner': [
+        "Roti",
+        "Kashmiri Pulao",
+        "Egg Bhurji Masala",
+        "Plain Dal",
+        "Rice",
+        "Rasam",
+        "Pickle"
+      ],
+    },
+    'Tuesday': {
+      'Breakfast': [
+        'Veg Paratha',
+        'Veg Sabji',
+        "Banava/Fruit Salad",
+        "Bread",
+        "Butter/Jam",
+        "Tea, Coffee, Milk"
+      ],
+      'Lunch': [
+        'Salad',
+        'Poori',
+        "Chana Masala",
+        "Plain Rice",
+        "Dal Tadka",
+        "Sambar",
+        "Rasam",
+        "Carrot Beans Poriyal",
+        "Curd"
+      ],
+      'Evening Snacks': ["Bhel/Chana Sundal", "Sauce", "Tea, Coffee, Milk"],
+      'Dinner': [
+        "Roti",
+        "Kuska",
+        "Aloo Meal Maker Sabji",
+        "Masala Dal",
+        "Rasam",
+        "Pickle",
+        "Halwa"
+      ],
+    },
+    'Wednesday': {
+      'Breakfast': [
+        'Veg Paratha',
+        'Veg Sabji',
+        "Banava/Fruit Salad",
+        "Bread",
+        "Butter/Jam",
+        "Tea, Coffee, Milk"
+      ],
+      'Lunch': [
+        'Salad',
+        'Poori',
+        "Chana Masala",
+        "Plain Rice",
+        "Dal Tadka",
+        "Sambar",
+        "Rasam",
+        "Carrot Beans Poriyal",
+        "Curd"
+      ],
+      'Evening Snacks': ["Bhel/Chana Sundal", "Sauce", "Tea, Coffee, Milk"],
+      'Dinner': [
+        "Roti",
+        "Kuska",
+        "Aloo Meal Maker Sabji",
+        "Masala Dal",
+        "Rasam",
+        "Pickle",
+        "Halwa"
+      ],
+    },
+    'Thursday': {
+      'Breakfast': [
+        'Veg Paratha',
+        'Veg Sabji',
+        "Banava/Fruit Salad",
+        "Bread",
+        "Butter/Jam",
+        "Tea, Coffee, Milk"
+      ],
+      'Lunch': [
+        'Salad',
+        'Poori',
+        "Chana Masala",
+        "Plain Rice",
+        "Dal Tadka",
+        "Sambar",
+        "Rasam",
+        "Carrot Beans Poriyal",
+        "Curd"
+      ],
+      'Evening Snacks': ["Bhel/Chana Sundal", "Sauce", "Tea, Coffee, Milk"],
+      'Dinner': [
+        "Roti",
+        "Kuska",
+        "Aloo Meal Maker Sabji",
+        "Masala Dal",
+        "Rasam",
+        "Pickle",
+        "Halwa"
+      ],
+    },
+    'Friday': {
+      'Breakfast': [
+        'Veg Paratha',
+        'Veg Sabji',
+        "Banava/Fruit Salad",
+        "Bread",
+        "Butter/Jam",
+        "Tea, Coffee, Milk"
+      ],
+      'Lunch': [
+        'Salad',
+        'Poori',
+        "Chana Masala",
+        "Plain Rice",
+        "Dal Tadka",
+        "Sambar",
+        "Rasam",
+        "Carrot Beans Poriyal",
+        "Curd"
+      ],
+      'Evening Snacks': ["Bhel/Chana Sundal", "Sauce", "Tea, Coffee, Milk"],
+      'Dinner': [
+        "Roti",
+        "Kuska",
+        "Aloo Meal Maker Sabji",
+        "Masala Dal",
+        "Rasam",
+        "Pickle",
+        "Halwa"
+      ],
+    },
+    'Saturday': {
+      'Breakfast': [
+        'Veg Paratha',
+        'Veg Sabji',
+        "Banava/Fruit Salad",
+        "Bread",
+        "Butter/Jam",
+        "Tea, Coffee, Milk"
+      ],
+      'Lunch': [
+        'Salad',
+        'Poori',
+        "Chana Masala",
+        "Plain Rice",
+        "Dal Tadka",
+        "Sambar",
+        "Rasam",
+        "Carrot Beans Poriyal",
+        "Curd"
+      ],
+      'Evening Snacks': ["Bhel/Chana Sundal", "Sauce", "Tea, Coffee, Milk"],
+      'Dinner': [
+        "Roti",
+        "Kuska",
+        "Aloo Meal Maker Sabji",
+        "Masala Dal",
+        "Rasam",
+        "Pickle",
+        "Halwa"
+      ],
+    },
+    'Sunday': {
+      'Breakfast': [
+        'Veg Paratha',
+        'Veg Sabji',
+        "Banava/Fruit Salad",
+        "Bread",
+        "Butter/Jam",
+        "Tea, Coffee, Milk"
+      ],
+      'Lunch': [
+        'Salad',
+        'Poori',
+        "Chana Masala",
+        "Plain Rice",
+        "Dal Tadka",
+        "Sambar",
+        "Rasam",
+        "Carrot Beans Poriyal",
+        "Curd"
+      ],
+      'Evening Snacks': ["Bhel/Chana Sundal", "Sauce", "Tea, Coffee, Milk"],
+      'Dinner': [
+        "Roti",
+        "Kuska",
+        "Aloo Meal Maker Sabji",
+        "Masala Dal",
+        "Rasam",
+        "Pickle",
+        "Halwa"
+      ],
+    },
+  };
+  print(days['Monday']!['Breakfast']);
 }
